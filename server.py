2e41a84d2fee817be9539ef7b5152203bc54ed93
pcranaway <pcranaway@tuta.io>
2021-01-26 21:30:18 +0200

    switch to a single file

--- server.py
+++ server.py
@@ -1,13 +1,44 @@
+import os
 from eve import Eve
 from eve.auth import BasicAuth
-from settings import ADMIN_PASSWORD, ADMIN_USERNAME
+
+MONGO_PORT        = int(os.environ['MONGO_PORT'])
+MONGO_HOST        = os.environ['MONGO_HOST']
+MONGO_USERNAME    = os.environ['MONGO_USERNAME']
+MONGO_PASSWORD    = os.environ['MONGO_PASSWORD']
+MONGO_AUTH_SOURCE = os.environ['MONGO_AUTH_SOURCE']
+MONGO_DBNAME      = os.environ['MONGO_DBNAME']
+
+ADMIN_USERNAME    = os.environ['ADMIN_USERNAME']
+ADMIN_PASSWORD    = os.environ['ADMIN_PASSWORD']
+
+profile_schema = {
+    'username': { 'type': 'string' },
+    'uuid': { 'type': 'string' }
+}
+
+profiles = {
+    'item_title': 'profile',
+    'additional_lookup': {
+        'url': 'regex("[\w]+")',
+        'field': 'username'
+    },
+    'resource_methods': ['GET', 'POST'],
+    'schema': profile_schema
+}
+
+domain = {
+    'profiles': profiles
+}
+
+settings =  {'DOMAIN': domain}
 
 class RestAuth(BasicAuth):
 
     def check_auth(self, username, password, allowed_roles, resoruce, method):
         return username == ADMIN_USERNAME and password == ADMIN_PASSWORD
 
-app = Eve(auth=RestAuth)
+app = Eve(auth=RestAuth, settings=settings)
 
 if __name__ == '__main__':
     app.run()
