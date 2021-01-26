35881433e336a1d01cc556bc7dbd664cd2b61f68
pcranaway <pcranaway@tuta.io>
2021-01-26 12:15:43 +0200

    initial commit

---
+++ settings.py
@@ -0,0 +1,27 @@
+import os
+
+MONGO_PORT        = int(os.environ['MONGO_PORT'])
+MONGO_HOST        = os.environ['MONGO_HOST']
+MONGO_USERNAME    = os.environ['MONGO_USERNAME']
+MONGO_PASSWORD    = os.environ['MONGO_PASSWORD']
+MONGO_AUTH_SOURCE = os.environ['MONGO_AUTH_SOURCE']
+MONGO_DBNAME      = os.environ['MONGO_DBNAME']
+
+profile_schema = {
+    'username': { 'type': 'string' },
+    'uuid': { 'type': 'string' }
+}
+
+profiles = {
+    'item_title': 'profile',
+    'additional_lookup': {
+        'url': 'regex("[\w]+")',
+        'field': 'username'
+    },
+    'resource_methods': ['GET', 'POST'],
+    'schema': profile_schema
+}
+
+DOMAIN = {
+    'profiles': profiles
+}
